d85038df0fd6a5400f27c91658f0f7970813eaf6
NIGHTINSIDE <contatopietroprofessional@gmail.com>
2024-02-03 03:49:35 -0300

    Adicionando validações sem tratamento para campos do body

--- src/main/java/br/com/agnesia/app/agnesia_application/modules/users/UserEntity.java
+++ src/main/java/br/com/agnesia/app/agnesia_application/modules/users/UserEntity.java
@@ -1,18 +1,26 @@
 package br.com.agnesia.app.agnesia_application.modules.users;
 
 import java.util.UUID;
+
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class UserEntity {
 
     private UUID id;
-    private String name;
-    private String username;
-    private String email;
     private String bio;
     private String uploadData;
+    private String name;
+
+    @Pattern(regexp = "^(?!\\s*$).+", message = "[username] field do not accept spaces")
+    private String username;
+    @Email(message = "[email] field only accetps valid emails")
+    private String email;
+    @Length(min = 8, max = 256)
     private String password;
 
-    
 }
--- src/main/java/br/com/agnesia/app/agnesia_application/modules/users/controllers/UserController.java
+++ src/main/java/br/com/agnesia/app/agnesia_application/modules/users/controllers/UserController.java
@@ -2,6 +2,8 @@ package br.com.agnesia.app.agnesia_application.modules.users.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 import br.com.agnesia.app.agnesia_application.modules.users.UserEntity;
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     @PostMapping("/")
-    public void create( @RequestBody UserEntity userEntity) {
+    public void create(@Valid @RequestBody UserEntity userEntity) {
 
         System.out.println("User");
         System.out.println(userEntity.getName());
