b4ebb1a9e033b200b2ed440d05fb18a15c69da1f
NIGHTINSIDE <contatopietroprofessional@gmail.com>
2024-02-07 15:14:15 -0300

    Adicionado ddl pra banco de dados e criacao automatica das entidades

--- src/main/java/br/com/agnesia/app/agnesia_application/modules/users/UserEntity.java
+++ src/main/java/br/com/agnesia/app/agnesia_application/modules/users/UserEntity.java
@@ -1,18 +1,26 @@
 package br.com.agnesia.app.agnesia_application.modules.users;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
-
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Entity(name = "agnuser")
 public class UserEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
     private String bio;
     private String uploadData;
     private String name;
@@ -22,7 +30,10 @@ public class UserEntity {
     private String username;
     @Email(message = "[email] field only accetps valid emails")
     private String email;
-    @Length(min = 8, max = 256)
+    @Length(min = 8, max = 256, message = "A senha deve conter um mínimo de (8) caracteres")
     private String password;
 
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+ 
 }
